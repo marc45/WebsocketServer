2d8fdbb6eb6e21349773f7f9869bc5e441e8b4f5
shabab koohi <sh.k.na.1368@gmail.com>
2017-09-05 12:03:13 +0430

    second commit befor tutorial

--- src/main/java/com/shabab/simplws/Application.java
+++ src/main/java/com/shabab/simplws/Application.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-import java.math.BigInteger;
-import java.net.InetAddress;
+
 import java.net.UnknownHostException;
 
 
@@ -17,20 +16,7 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) throws UnknownHostException {
         SpringApplication.run(Application.class, args);
 
-        testIp();
-    }
-    private static void testIp() throws UnknownHostException {
-
-        long ipAddress = new BigInteger(InetAddress.getByName("176.9.251.198").getAddress()).longValue();
-
-        System.out.println("By String IP address: \n" +
-                GeoIPv4.getLocation("176.9.251.198"));
-
-        System.out.println("By long IP address: \n" +
-                GeoIPv4.getLocation(ipAddress));
-
-        System.out.println("By InetAddress IP address: \n" +
-                GeoIPv4.getLocation(InetAddress.getByName("176.9.251.198")));
 
     }
+
     }
